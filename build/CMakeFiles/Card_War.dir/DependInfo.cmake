267c5998271db2d33bc22451254fcc0a8a6e093b
Abhijit <abhijitabhi127@gmail.com>
2025-05-31 11:35:45 +0530

    corrected the implementation and tested and the deck portion of the project is working fine
    there was some error with the deck class where i was accessing deck members in constructor of the deck itself which was resolved by making said members static. as they were made static they couldn't be defined there itself unless i made them inline static which i didn't do. what i did was defining them in the Deck.cpp where there are rest of the definitons

---
+++ build/CMakeFiles/Card_War.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/WORKING_FOLDER/PROJECTS/WAR/Card_War/main.cpp" "CMakeFiles/Card_War.dir/main.cpp.obj" "gcc" "CMakeFiles/Card_War.dir/main.cpp.obj.d"
+  "C:/WORKING_FOLDER/PROJECTS/WAR/Card_War/src/Deck.cpp" "CMakeFiles/Card_War.dir/src/Deck.cpp.obj" "gcc" "CMakeFiles/Card_War.dir/src/Deck.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Card_War.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Card_War.dir/main.cpp.obj"
+  "CMakeFiles/Card_War.dir/main.cpp.obj.d"
+  "CMakeFiles/Card_War.dir/src/Deck.cpp.obj"
+  "CMakeFiles/Card_War.dir/src/Deck.cpp.obj.d"
+  "Card_War.exe"
+  "Card_War.exe.manifest"
+  "Card_War.pdb"
+  "libCard_War.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Card_War.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
